3e526ffa3298db8589622f80db1d6b1f74f91ecb
Raymond Gauthier <jraygauthier@gmail.com>
2020-11-13 21:47:22 -0500

    Some todo and obtain typed user

--- Data/WeatherForecastService.cs
+++ Data/WeatherForecastService.cs
@@ -2,6 +2,8 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
 
 namespace blazor_auth_individual_experiment.Data
 {
@@ -13,16 +15,24 @@ namespace blazor_auth_individual_experiment.Data
         };
 
         AuthenticationStateProvider _authStateProvider;
+        UserManager<IdentityUser> _userMgr;
 
-        public WeatherForecastService(AuthenticationStateProvider authStateProvider)
+        public WeatherForecastService(
+            AuthenticationStateProvider authStateProvider,
+            UserManager<IdentityUser> userMgr)
         {
             _authStateProvider = authStateProvider;
+            _userMgr = userMgr;
         }
 
         public async Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
         {
+            // TODO: Move this into a separte current user service which
+            // can provided either only the username / id (fast) and if
+            // required the whole typed user.
             var state = await _authStateProvider.GetAuthenticationStateAsync();
             var userName = state.User.Identity.Name;
+            var user = await _userMgr.GetUserAsync(state.User);
 
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
